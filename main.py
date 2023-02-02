f1d83ee9b1e42b61a252b23a61a7beb18ef571d6
RFKolor <https://github.com/RFKolor/Gus-Hrustalniy-Babaskin.git>
2023-02-02 16:40:14 +0300

    ура ура ура мы сделали первую часть!!!!

--- main.py
+++ main.py
@@ -17,42 +17,50 @@ class Widget(QWidget):
         self.choose_btn.setText("Поиск")
         self.x_line.resize(100, 50)
         self.y_line.resize(100, 50)
+        self.image = QLabel(self)
+        self.x_coords = QLabel(self)
+        self.y_coords = QLabel(self)
+        self.x_coords.setText("x")
+        self.y_coords.setText("y")
+        self.x_coords.resize(50, 50)
+        self.y_coords.resize(50, 50)
+        self.x_coords.move(105, 400)
+        self.y_coords.move(105, 450)
         self.x_line.move(0, 400)
         self.y_line.move(0, 450)
+        self.x_line.setText("45")
+        self.y_line.setText("55")
         self.choose_btn.resize(100, 50)
         self.choose_btn.move(200, 425)
         self.do_image()
-        self.pixmap = QPixmap('res.png')
-        # Если картинки нет, то QPixmap будет пустым,
-        # а исключения не будет
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(500, 400)
-        # Отображаем содержимое QPixmap в объекте QLabel
-        self.image.setPixmap(self.pixmap)
+        self.choose_btn.clicked.connect(self.do_image)
 
     def initUI(self):
         self.setGeometry(200, 200, 500, 500)
         self.setWindowTitle("Яндекс карты")
 
     def do_image(self):
-        toponym_coodrinates = '45 55'
-        toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
-
-        delta = "0.005"
-
-        map_params = {
-            "ll": ",".join([toponym_longitude, toponym_lattitude]),
-            "spn": ",".join([delta, delta]),
-            "l": "map"
-        }
-
-        map_api_server = "http://static-maps.yandex.ru/1.x/"
-        response = requests.get(map_api_server, params=map_params)
-
-        im = Image.open(BytesIO(
-            response.content))
-        im.save('res.png')
+        try:
+            toponym_coodrinates = f"{self.x_line.text()} {self.y_line.text()}"
+            toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+            delta = "0.005"
+            map_params = {
+                "ll": ",".join([toponym_longitude, toponym_lattitude]),
+                "spn": ",".join([delta, delta]),
+                "l": "map"
+            }
+            map_api_server = "http://static-maps.yandex.ru/1.x/"
+            response = requests.get(map_api_server, params=map_params)
+            im = Image.open(BytesIO(
+                response.content))
+            im.save('res.png')
+            #мой код,а не его
+            self.pixmap = QPixmap('res.png')
+            self.image.move(0, 0)
+            self.image.resize(500, 400)
+            self.image.setPixmap(self.pixmap)
+        except Exception:
+            print("Не надо ломать программу")
 
 
 if __name__ == "__main__":
