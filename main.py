0473f3359ecf29cb30590601af1032b64feec71e
RFKolor <https://github.com/RFKolor/Gus-Hrustalniy-Babaskin.git>
2023-02-02 16:03:49 +0300

    кнопочки

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit
 import sys
+import requests
 
 
 class Widget(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
+        self.x_line = QLineEdit(self)
+        self.y_line = QLineEdit(self)
+        self.choose_btn = QPushButton(self)
+        self.choose_btn.setText("Поиск")
+        self.x_line.resize(100, 50)
+        self.y_line.resize(100, 50)
+        self.x_line.move(0, 400)
+        self.y_line.move(0, 450)
+        self.choose_btn.resize(100, 50)
+        self.choose_btn.move(200, 425)
 
     def initUI(self):
         self.setGeometry(200, 200, 500, 500)
